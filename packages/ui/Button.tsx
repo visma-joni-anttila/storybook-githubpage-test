ac70c9118da986da04ccc7fa8c42302c74848d83
Joni Anttila <joni.anttila@visma.com>
2023-04-13 09:03:07 +0300

    Storybook setup

--- packages/ui/Button.tsx
+++ packages/ui/Button.tsx
@@ -1,5 +1,22 @@
-import * as React from "react";
+interface Props {
+  primary?: boolean;
+  size?: "small" | "large";
+  label?: string;
+}
 
-export const Button = () => {
-  return <button>Boop</button>;
+export const Button = ({
+  primary = false,
+  label = "Boop",
+  size = "small",
+}: Props) => {
+  return (
+    <button
+      style={{
+        backgroundColor: primary ? "red" : "blue",
+        fontSize: size === "large" ? "24px" : "14px",
+      }}
+    >
+      {label}
+    </button>
+  );
 };
